5a5860f4d130fc56d23e0505233d60aa130d17da
Bastian Noffer <bastian.noffer@brandung.de>
2021-09-03 12:16:49 +0200

    setup the Android TestFixture to use the APK from the android build folder

--- reactdemoapp-uitest/reactdemoapp-uitest.Droid/Tests.cs
+++ reactdemoapp-uitest/reactdemoapp-uitest.Droid/Tests.cs
@@ -19,14 +19,9 @@ namespace reactdemoappuitest
         [SetUp]
         public void BeforeEachTest()
         {
-            // TODO: If the Android app being tested is included in the solution then open
-            // the Unit Tests window, right click Test Apps, select Add App Project
-            // and select the app projects that should be tested.
             app = ConfigureApp
                 .Android
-                // TODO: Update this path to point to your Android app and uncomment the
-                // code if the app is not included in the solution.
-                //.ApkFile ("../../../Android/bin/Debug/UITestsAndroid.apk")
+                .ApkFile("../../../../reactdemoapp/android/app/build/outputs/apk/debug/app-debug.apk")
                 .StartApp();
         }
     }
